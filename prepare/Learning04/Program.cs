3ed69693400159aeafd416d92459d2f7d23a26bb
John Obiko <obi22004@byui.edu>
2023-06-29 10:48:43 +0300

    week 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,27 @@
+// Program.cs
+
 using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a MathAssignment object
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+
+        // Display Math assignment summary
+        Console.WriteLine(mathAssignment.GetSummary());
+
+        // Display Math homework list
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Create a WritingAssignment object
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        // Display Writing assignment summary
+        Console.WriteLine(writingAssignment.GetSummary());
+
+        // Display Writing assignment information
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
