217a78250f355f4b08071b99ffa9b89c2ae13a79
John obiko <obi22004@byui.edu>
2023-07-16 01:58:18 +0300

    changes

--- final/FinalProject/FileHandler.cs
+++ final/FinalProject/FileHandler.cs
@@ -13,11 +13,22 @@ namespace TaskManagementSystem
             this.filePath = filePath;
         }
 
-        public FileHandler()
+        public void SaveToFile(TaskManager taskManager)
         {
+            // Implementation for SaveToFile method
+            // ...
+
+            // Call the WriteToFile method
+            WriteToFile(taskManager);
         }
 
-        public void SaveToFile(TaskManager taskManager)
+        public void LoadFromFile(TaskManager taskManager)
+        {
+            // Implementation for LoadFromFile method
+            // ...
+        }
+
+        public void WriteToFile(TaskManager taskManager)
         {
             try
             {
@@ -37,54 +48,6 @@ namespace TaskManagementSystem
             {
                 Console.WriteLine($"Error while saving tasks to file: {e.Message}");
             }
-        }
-
-        public void LoadFromFile(TaskManager taskManager)
-        {
-            try
-            {
-                if (File.Exists(filePath))
-                {
-                    using (StreamReader reader = new StreamReader(filePath))
-                    {
-                        string line;
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            string[] taskData = line.Split(',');
-
-                            string taskID = taskData[0];
-                            string taskName = taskData[1];
-                            string description = taskData[2];
-                            DateTime deadline = DateTime.Parse(taskData[3]);
-                            int priority = int.Parse(taskData[4]);
-                            string status = taskData[5];
-
-                            Task task = new Task(taskID, taskName, description, deadline, priority, status);
-                            taskManager.AddTask(task);
-                        }
-                    }
-
-                    Console.WriteLine("Tasks loaded from file successfully!");
-                }
-                else
-                {
-                    Console.WriteLine("No file found. No tasks loaded.");
-                }
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine($"Error while loading tasks from file: {e.Message}");
-            }
-        }
-
-        internal void ReadFromFile(TaskManager taskManager)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void WriteToFile(TaskManager taskManager)
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -8,68 +8,52 @@ namespace TaskManagementSystem
         {
             TaskManager taskManager = new TaskManager();
             UserInterface userInterface = new UserInterface();
-            FileHandler fileHandler = new FileHandler();
+            FileHandler fileHandler = new FileHandler("tasks.txt");
 
             // Load tasks from file (if available)
-            fileHandler.ReadFromFile(taskManager);
+            fileHandler.LoadFromFile(taskManager);
 
-            bool isRunning = true;
-
-            while (isRunning)
+            bool exit = false;
+            while (!exit)
             {
-                Console.Clear();
                 userInterface.DisplayMenu();
-
                 Console.Write("Enter your choice: ");
-                string userInput = Console.ReadLine();
+                string choice = Console.ReadLine();
 
-                switch (userInput)
+                switch (choice)
                 {
                     case "1":
-                        Console.WriteLine("Creating a new task:");
                         Task newTask = userInterface.CreateTask();
                         taskManager.AddTask(newTask);
-                        Console.WriteLine("Task created successfully!");
                         break;
                     case "2":
-                        Console.WriteLine("Assigning priority to a task:");
                         userInterface.AssignPriority(taskManager);
                         break;
                     case "3":
-                        Console.WriteLine("Setting deadline for a task:");
                         userInterface.SetDeadline(taskManager);
                         break;
                     case "4":
-                        Console.WriteLine("Viewing task details:");
                         userInterface.ViewTaskDetails(taskManager);
                         break;
                     case "5":
-                        Console.WriteLine("Saving tasks to file...");
                         fileHandler.SaveToFile(taskManager);
-                        Console.WriteLine("Tasks saved successfully!");
                         break;
                     case "6":
-                        Console.WriteLine("Loading tasks from file...");
                         fileHandler.LoadFromFile(taskManager);
-                        Console.WriteLine("Tasks loaded successfully!");
                         break;
                     case "0":
-                        isRunning = false;
+                        exit = true;
                         break;
                     default:
-                        Console.WriteLine("Invalid input. Please try again.");
+                        Console.WriteLine("Invalid choice. Please try again.");
                         break;
                 }
-
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
             }
 
             // Save tasks to file before exiting
             fileHandler.WriteToFile(taskManager);
 
-            Console.WriteLine("Task Management System has exited. Press any key to close the program...");
-            Console.ReadKey();
+            Console.WriteLine("Task Management System exited. Goodbye!");
         }
     }
 }
